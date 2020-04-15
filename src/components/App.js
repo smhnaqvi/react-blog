08bdd3b572732b360fc56cd5b10118702aa6ea65
smhnaqvi111 <smhanqvi111@gmail.com>
2020-04-15 09:25:31 +0430

    init blog project -add route Home -add component Home

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import logo from '../logo.svg';
+import Home from './Home';
+
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Home} />
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
